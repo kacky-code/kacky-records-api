358807d98f3c3931ac885ced4b9effdec0519ac7
Daniel Bremer <5bremer@informatik.uni-hamburg.de>
2022-12-20 03:43:32 +0100

    now tracking KR

--- src/kacky_records_api/update_records.py
+++ src/kacky_records_api/update_records.py
@@ -20,7 +20,6 @@ kacky_reloaded_lock = Lock()
 
 
 def build_score(
-    kid: str,
     score: int,
     date: Union[str, dt],
     source: str,
@@ -28,6 +27,7 @@ def build_score(
     nick: Union[str, None] = None,
     tmx_id: Union[str, int, None] = None,
     tm_uid: Union[str, None] = None,
+    kid: str = "",
 ) -> Dict[str, Union[str, int]]:
     # either tm_uid or tmx_id need to be set
     if not (tm_uid or tmx_id):
@@ -40,7 +40,6 @@ def build_score(
         raise ValueError("Bad value for source")
     # build base result dict (missing tmx_id/tm_uid
     res = {
-        "kid": kid if "#" not in kid else kid.split("#")[1].replace("\u2013", "-"),
         "score": score,
         "date": date if isinstance(date, str) else date.strftime("%Y-%m-%d %H:%M:%S"),
         "source": source,
@@ -55,10 +54,13 @@ def build_score(
         res["login"] = login
     if nick:
         res["nick"] = nick
+    # add kacky id if given
+    if kid:
+        res["kid"] = kid if "#" not in kid else kid.split("#")[1].replace("\u2013", "-")
     return res
 
 
-def check_new_scores(candidates, src: str):
+def check_new_scores(candidates, src: str, config, secrets):
     update_elements = []
 
     # set up connection to backend database
@@ -74,8 +76,8 @@ def check_new_scores(candidates, src: str):
     if src == "tmx":
         query = basequery + "tmx_id = ?;"
         for kid, data in candidates.items():
-            a = backend_db.fetchall(query, (data["wrscore"], data["tid"]))
-            if a:
+            check_score = backend_db.fetchall(query, (data["wrscore"], data["tid"]))
+            if check_score:
                 date = (
                     dt.strptime(data["lastactivity"], "%Y-%m-%dT%H:%M:%S.%f")
                     if "lastactivity" in data
@@ -83,29 +85,35 @@ def check_new_scores(candidates, src: str):
                 )
                 update_elements.append(
                     build_score(
-                        kid,
                         data["wrscore"],
                         date,
                         "TMX",
                         nick=data["wruser"],
                         tmx_id=data["tid"],
+                        kid=kid,
                     )
                 )
     elif src == "kkdb":
         query = basequery + "tm_uid = ?;"
-        for e in candidates:
-            a = backend_db.fetchall(query, (e[4], e[0]))
-            if a:
+        for candidate in candidates:
+            check_score = backend_db.fetchall(query, (candidate[4], candidate[0]))
+            if check_score:
                 update_elements.append(
                     build_score(
-                        e[1], e[4], e[5], "KKDB", login=e[6], nick=e[7], tm_uid=e[0]
+                        candidate[4],
+                        candidate[5],
+                        "KKDB",
+                        login=candidate[6],
+                        nick=candidate[7],
+                        tm_uid=candidate[0],
+                        kid=candidate[1],
                     )
                 )
     elif src == "dedi":
         query = basequery + "tmx_id = ?;"
         for kid, data in candidates.items():
-            a = backend_db.fetchall(query, (data["wrscore"], data["tid"]))
-            if a:
+            check_score = backend_db.fetchall(query, (data["wrscore"], data["tid"]))
+            if check_score:
                 date = (
                     dt.strptime(data["lastactivity"], "%Y-%m-%d %H:%M:%S")
                     if "lastactivity" in data
@@ -113,25 +121,38 @@ def check_new_scores(candidates, src: str):
                 )
                 update_elements.append(
                     build_score(
-                        kid,
                         data["wrscore"],
                         date,
                         "DEDI",
                         nick=data["wruser"],
                         tmx_id=data["tid"],
+                        kid=kid,
                     )
                 )
     elif src == "kkdb":
         query = basequery + "tm_uid = ?;"
-        for e in candidates:
-            a = backend_db.fetchall(query, (e[4], e[0]))
-            if a:
+        for candidate in candidates:
+            check_score = backend_db.fetchall(query, (candidate[4], candidate[0]))
+            if check_score:
                 update_elements.append(
                     build_score(
-                        e[1], e[4], e[5], "KKDB", login=e[6], nick=e[7], tm_uid=e[0]
+                        candidate[4],
+                        candidate[5],
+                        "KKDB",
+                        login=candidate[6],
+                        nick=candidate[7],
+                        tm_uid=candidate[0],
+                        kid=candidate[1],
                     )
                 )
-
+    elif src == "nado":
+        query = basequery + "tm_uid = ?;"
+        for candidate in candidates:
+            check_score = backend_db.fetchall(
+                query, (candidate["score"], candidate["tm_uid"])
+            )
+            if check_score:
+                update_elements.append(candidate)
     return update_elements
 
 
@@ -196,10 +217,10 @@ def update_wrs_kackiest_kacky(config, secrets):
         all_dedi_wrs = {}
 
     update_wrs_kk = []
-    update_wrs_kk += check_new_scores(recent_wrs_kk_db, "kkdb")
-    update_wrs_kk += check_new_scores(recent_wrs_kk_tmx, "tmx")
+    update_wrs_kk += check_new_scores(recent_wrs_kk_db, "kkdb", config, secrets)
+    update_wrs_kk += check_new_scores(recent_wrs_kk_tmx, "tmx", config, secrets)
     # update_wrs_kk += check_new_scores(all_tmx, "tmx")
-    update_wrs_kk += check_new_scores(all_dedi_wrs, "dedi")
+    update_wrs_kk += check_new_scores(all_dedi_wrs, "dedi", config, secrets)
     update_wrs_kk_dedup = dedup_new_scores(update_wrs_kk)
 
     # set up connection to backend database
@@ -292,7 +313,7 @@ def update_wrs_kacky_reloaded(config, secrets):
         club_campaings[reloaded_update_counter]["campaignId"],
     )
     campaign_maps = campaing_info["campaign"]["playlist"]
-    print(campaign_maps)
+    print(campaing_info["name"])
 
     if secrets["credentials_type"] == "account":
         nadeo_serv = NadeoServices(
@@ -310,8 +331,60 @@ def update_wrs_kacky_reloaded(config, secrets):
         )
     else:
         raise ValueError("Bad Value for 'credentials_type' in secrets.yaml")
-    print(nadeo_serv.get_account_display_name("3d803f03-f99a-4784-ad5e-a28ab3740dd3"))
-    return
+
+    scores = []
+    for cmap in campaign_maps:
+        mapscore = nadeo_live_serv.get_worldrecord_for_map(cmap["mapUid"])["tops"][0][
+            "top"
+        ][0]
+        player = nadeo_serv.get_account_display_name(mapscore["accountId"])[0]
+        scores.append(
+            build_score(
+                mapscore["score"],
+                dt.now(),
+                "NADO",
+                login=player["displayName"],
+                tm_uid=cmap["mapUid"],
+            )
+        )
+
+    update_scores = check_new_scores(scores, "nado", config, secrets)
+    print(f"found {len(scores)} wrs. updating {len(update_scores)}.")
+    # no deduplication needed, as we have only one data source
+
+    # set up connection to backend database
+    backend_db = DBConnection(config, secrets)
+
+    for new_wr in update_scores:
+        print(f"updating in DB: {new_wr}")
+        query_discord = """
+                    UPDATE worldrecords_discord_notify AS wr_not
+                    LEFT JOIN worldrecords AS wr
+                        ON wr_not.id = wr.id
+                    LEFT JOIN maps
+                        ON wr.map_id = maps.id
+                    SET notified = 0, time_diff = wr.score - ?
+                    WHERE maps.tm_uid = ?;
+                    """
+        backend_db.execute(query_discord, (new_wr["score"], new_wr["tm_uid"]))
+        query = """
+                 UPDATE worldrecords AS wr
+                 LEFT JOIN maps
+                 ON wr.map_id = maps.id
+                 SET score = ?, login = ?, nickname = ?, source = ?, date = ?
+                 WHERE maps.tm_uid = ?;
+                 """
+        backend_db.execute(
+            query,
+            (
+                new_wr["score"],
+                new_wr["login"] if "login" in new_wr else "",
+                new_wr["nick"] if "nick" in new_wr else "",
+                new_wr["source"],
+                new_wr["date"],
+                new_wr["tm_uid"],
+            ),
+        )
 
     reloaded_update_counter = (reloaded_update_counter + 1) % len(club_campaings)
     kacky_reloaded_lock.release()
